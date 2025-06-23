6cb0683f6a0d8f2a0441ebe9441bd64b2c333d9f
Ahmadhassan21 <ah7232374@gmail.com>
2025-06-23 10:32:09 +0500

    Added [feature/fix] to project

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -108,7 +108,7 @@ const CartPage = () => {
                   </span>
                   <a
                     href="#"
-                    title
+                    title="Continue Shopping"
                     className="inline-flex items-center gap-2 text-sm font-medium text-white underline hover:no-underline "
                   >
                     Continue Shopping
@@ -144,7 +144,7 @@ const CartPage = () => {
                       type="text"
                       id="voucher"
                       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
-                      placeholder
+                      placeholder="Enter your voucher code"
                       required
                     />
                   </div>
